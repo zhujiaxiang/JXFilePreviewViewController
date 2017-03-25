f6a62e666392c4b19f64de3dcdff13223f4ff60d
zhujiaxiang <zjxbaozoudhm@gmail.com>
2017-03-25 16:44:25 +0800

    ready to resumeDownload

---
+++ JXFilePreviewViewController/JXResumeData.h
@@ -0,0 +1,13 @@
+//
+//  JXResumeData.h
+//  JXFilePreviewViewController
+//
+//  Created by 朱佳翔 on 2017/3/25.
+//  Copyright © 2017年 zjx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JXResumeData : NSObject
+
+@end
---
+++ JXFilePreviewViewController/JXResumeData.m
@@ -0,0 +1,13 @@
+//
+//  JXResumeData.m
+//  JXFilePreviewViewController
+//
+//  Created by 朱佳翔 on 2017/3/25.
+//  Copyright © 2017年 zjx. All rights reserved.
+//
+
+#import "JXResumeData.h"
+
+@implementation JXResumeData
+
+@end
